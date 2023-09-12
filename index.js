d04a2ebab986026160c600e97db52e2285e10cdf
mrnayaa <malam563@newschool.edu>
2023-09-12 00:06:25 -0400

    entry 1

---
+++ index.js
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+ <html>
+   <head>
+   <meta charset="utf-8" />
+   <meta name="viewport" content="width=device-width"></meta>
+     <title>composition</title>
+   </head>
+   <body>
+     <h1> <strong>everything</strong> is made of something</h1>
+     <p id="whitespace">and
+
+    can be broken down
+
+        into 
+        
+        components.</p>
+     <ul>
+	  <li>human body</li>
+	  <li>inanimate objects</li>
+	  <li>concepts</li>
+  </ul>
+  <hr>
+  <h2></h2>
+  <!-- <p>Everything is made of something and can be broken into components. 
+    The human body is made up of different organs - lungs, heart, and brain. 
+    If we zoom in, we see smaller parts - different types of cells. 
+    However, at the most basic level, everything living and non-living consists of atoms, 
+    and they form into larger structures - molecules. 
+    Suppose we stop basing our perceptions on the rules of physics and chemistry 
+    and look at the world not from a scientific point of view but from an artistic perspective. 
+    In that case, we get a sense of diversity and freedom to determine 
+    what the world around us consists of. My theme explores the concept of composition 
+    through the examples of inanimate objects, phenomenons, and other non-physical products,
+    such as texts, books, and articles. I plan to update or create my entries 
+    based on what I see or interact with daily. 
+    Our environment is captivating and exciting to study and document, 
+    and each entry I submit will dive deeper into this theme. </p> -->
+   </body>
+ </html>
