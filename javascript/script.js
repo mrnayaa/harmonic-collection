070f6dcc49e4715b381535e4a8cfc791ae806828
mrnayaa <malam563@newschool.edu>
2023-10-21 00:23:17 -0400

    g
    g

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,16 @@
---
+document.addEventListener("DOMContentLoaded", function() {
+    // Get a reference to the scroll button
+    const scrollButton = document.getElementById("scrollButton");
+
+    // Define the target position (1000px from the top)
+    const targetPosition = 1000;
+
+    // Add a click event listener to the button
+    scrollButton.addEventListener("click", function() {
+        // Use the smooth scrolling behavior
+        window.scrollTo({
+            top: targetPosition,
+            behavior: "smooth"
+        });
+    });
+});
--- javascript/script6.js
+++
@@ -1,8 +0,0 @@
-<script>
-    const imageContainer = document.querySelector('.image-container');
-    const hiddenText = document.querySelector('.hidden-text');
-
-    imageContainer.addEventListener('click', () => {
-        hiddenText.style.display = 'block';
-    });
-</script>
